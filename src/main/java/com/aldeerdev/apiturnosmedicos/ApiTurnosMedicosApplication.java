04ba7152b00c9cb83bffacdf0edca85fb3c2fc62
AldeerDev <aldeerdev@gmail.com>
2026-02-06 08:50:22 -0300

    Primer commit

---
+++ src/main/java/com/aldeerdev/apiturnosmedicos/ApiTurnosMedicosApplication.java
@@ -0,0 +1,13 @@
+package com.aldeerdev.apiturnosmedicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTurnosMedicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTurnosMedicosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aldeerdev/apiturnosmedicos/ApiTurnosMedicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aldeerdev.apiturnosmedicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTurnosMedicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
