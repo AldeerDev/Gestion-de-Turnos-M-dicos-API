a969f72ca9db843e8995d3da038d8e65ef91aed5
AldeerDev <aldeerdev@gmail.com>
2026-02-09 12:10:41 -0300

    Agrega getId y setId de la clase Medico

--- src/main/java/com/aldeerdev/apiturnosmedicos/model/Medico.java
+++ src/main/java/com/aldeerdev/apiturnosmedicos/model/Medico.java
@@ -32,6 +32,14 @@ public class Medico {
 		this.activo = activo;
 	}
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	public String getNombre() {
 		return nombre;
 	}
