a0e25e02104b66b9d6d0e37b0e2f55cc0ae5becc
AldeerDev <aldeerdev@gmail.com>
2026-02-11 21:44:23 -0300

    refactor: cambiar mensaje de excepcion mas generalizada

--- src/main/java/com/aldeerdev/apiturnosmedicos/exception/MedicoOcupadoException.java
+++ src/main/java/com/aldeerdev/apiturnosmedicos/exception/MedicoOcupadoException.java
@@ -3,6 +3,6 @@ package com.aldeerdev.apiturnosmedicos.exception;
 public class MedicoOcupadoException extends RuntimeException{
 
 	public MedicoOcupadoException() {
-		super("El médico ya tiene un turno en esa fecha y hora");
+		super("El medico ya tiene turno en ese horario.");
 	}
 }
